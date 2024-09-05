57642ab5a27da5555dabe92d07561099cefdc4eb
Kashish patil <156797313+kashishpatil1218@users.noreply.github.com>
2024-09-05 14:23:04 +0530

    Add files via upload

---
+++ lib/utils/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../screen/addpage.dart';
+import '../screen/edit_page.dart';
+import '../screen/homePage.dart';
+import '../screen/search_page.dart';
+import '../screen/splash_page.dart';
+
+Map<String, Widget Function(BuildContext context)> routes = {
+  '/': (context) => const SplashPage(),
+  '/add': (context) => const Addpage(),
+  '/home': (context) => const Homepage(),
+  '/search': (context) => const Searchpage(),
+  '/edit': (context) => const Editpage(),
+
+};
